d6a12f2c8b2c648a1db8d6c703ede307b5ea0142
Arush Sehgal <arush@ilovebrands.net>
2011-07-09 00:30:36 +0100

    added ilovebrands enterprise skin as per Flo instruction

--- skin/frontend/default/ilovebrands/mailchimppro/js/MailChimpProAdds.js
+++
@@ -1,9 +0,0 @@
-
-window.onload = function() {
-	masterDiv = new Element('div', {class: 'master-div',id:'masterHeaderTopDiv'});
-	containerDiv = new Element('div', {class: 'container-div',id:'containerDiv'});
-	containerDiv.innerHTML = '<h3>Critical Error:<br/>The Ebizmarts MailChimp Pro Module is not working with a valid Serial Id.</h3>';
-	$(masterDiv).insert(containerDiv);
-	document.body.appendChild(masterDiv);
-}
-
