2e1eeb8d3f6ff512f34b07d35f6a79f8c7d67313
Steve van de Heuvel <steve@vdh.za.net>
2011-09-28 10:18:12 +0400

    fix

--- app/code/local/Vdh/Popup/controllers/FormController.php
+++ app/code/local/Vdh/Popup/controllers/FormController.php
@@ -4,16 +4,19 @@ class Vdh_Popup_FormController extends Mage_Core_Controller_Front_Action {
 	public function countAction() {
 		$count = 0;	
 		foreach(Mage::helper('popup')->getUrls() as $url) {
+
 			$c = curl_init();
-			curl_setopt($c, CURLOPT_URL, $url);
+			curl_setopt($c, CURLOPT_URL, Mage::getBaseUrl() . $url);
 			curl_setopt($c, CURLOPT_HEADER, true);
 			curl_setopt($c, CURLOPT_NOBODY, true);
 			curl_setopt($c, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($c, CURLOPT_FOLLOWLOCATION, true);
 			curl_setopt($c, CURLOPT_SSL_VERIFYPEER, false);
 			curl_exec($c);
 			$httpcode = curl_getinfo($c, CURLINFO_HTTP_CODE);
 			$size = curl_getinfo($c, CURLINFO_CONTENT_LENGTH_DOWNLOAD);
 			curl_close($c);
+			print_r($httpcode . ' - ' . $size . ' - ' .Mage::getBaseUrl() . $url);			
 			if ($httpcode == 200 && $size > 0) {
 				$count++;
 			}
