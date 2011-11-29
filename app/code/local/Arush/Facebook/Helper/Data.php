f92a2f3ceacbf83c4b162cf94aa3b16a65562d7e
evoguaa3 terminal <arush@ilovebrands.net>
2011-11-29 15:48:50 +0000

    facebook canvas fixing

--- app/code/local/Arush/Facebook/Helper/Data.php
+++ app/code/local/Arush/Facebook/Helper/Data.php
@@ -9,20 +9,29 @@ class Arush_Facebook_Helper_Data extends Mage_Core_Helper_Abstract{
 	
 	
 	public function parsePageSignedRequest() {
+	    $secret = '99d4debf0157da44e0609e6f305d756f';
+	    
 	    if (isset($_REQUEST['signed_request'])) {
 	      $encoded_sig = null;
 	      $payload = null;
 	      list($encoded_sig, $payload) = explode('.', $_REQUEST['signed_request'], 2);
 	      $sig = base64_decode(strtr($encoded_sig, '-_', '+/'));
 	      $data = json_decode(base64_decode(strtr($payload, '-_', '+/'), true));
-	      return $data;
+	      
+	      $expected_sig = hash_hmac('sha256', $payload, $secret, $raw = true);
+	  
+	  if ($sig !== $expected_sig) {
+	    echo 'bad signed JSON signature!';
+	    error_log('Bad Signed JSON signature!');
+	    return null;
+	  } else {  return $data;}
+	  
 	    }
 	    return false;
 	}
 
-	public function fbApiIsFan($auth_token, $user_id) {
-		$apiUrl = 'https://graph.facebook.com/' . $user_id . '/likes?access_token=' . $auth_token;
-		
+	public function fbApiCall($apiUrl) {
+				
 		$result = "rpxCallUrl: no result yet";
 
         try {
@@ -60,11 +69,12 @@ $OAdomain = $this->getOneallApiDomain();
 		    {
 		        //Close connection
 		        curl_close($curl);		
-	
+
 	            try {
 	               // if you're debugging, this is where you can see the json string
-	               // print_r($json);
+	               //print_r($json);
 	               $result = json_decode($json);  
+	               
 	            }
 	            catch (Exception $e) {
 	                throw Mage::exception('Mage_Core', $e);
@@ -90,19 +100,36 @@ $OAdomain = $this->getOneallApiDomain();
 	public function isFan() {
 		$isFan = false;
 		if($signed_request = $this->parsePageSignedRequest()) {
-		    $auth_token = $signed_request->oauth_token;
-		    $user_id = $signed_request->user_id;
+		    if(isset($signed_request->page->liked)) {
+		    	if($signed_request->page->liked == 1) {
+		    		$isFan = true;
+		    		return $isFan;
+		    	}
+		    	else {return $isFan;}
+		    }
 		    
-		    if($auth_token) {
-			
-			
-				$userLikes = $this->fbApiIsFan($auth_token, $user_id);
+		    else {
+					// print_r($signed_request);
+					//print_r($signed_request);
+				}
+		}
+				
+			else {echo 'something went really wrong';}
+
+	}
+	
+	public function buildApiRequest($signed_request) {
+		if(isset($signed_request->oauth_token)) {
+		    	
+		    	$auth_token = $signed_request->oauth_token;
+		    	$user_id = $signed_request->user_id;
+		    	$apiUrl = 'https://graph.facebook.com/' . $user_id . '/likes?access_token=' . $auth_token;
+		    	$userLikes = $this->fbApiIsFan($apiUrl);
 				$numLikes = count($userLikes->data);
 				// echo $numLikes;				
 				$i = 0;
 				$fbPageId = $this->getFbPageId();
-				
-				
+
 				while($isFan == false && $i < $numLikes) {
 					if($userLikes->data[$i]->id == $fbPageId) {
 						$isFan = true;
@@ -110,14 +137,8 @@ $OAdomain = $this->getOneallApiDomain();
 					}
 					$i++;
 				}
-				
-				
-			} else {
-						echo "Please click on the Like button to view this tab!";
-					}
-				}
-				
-			else {print_r($signed_request);}
+		    }
+		 else {echo 'No auth token';}
 	}
 
 } 
