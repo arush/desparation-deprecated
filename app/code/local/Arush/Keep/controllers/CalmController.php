828c923fed6ae282b2aadfe28784fbfd89715336
Arush Sehgal <arush@ilovebrands.net>
2011-12-01 12:06:40 +0000

    fix facebook frame temporarily

---
+++ app/code/local/Arush/Keep/controllers/CalmController.php
@@ -0,0 +1,9 @@
+<?php
+
+class Arush_Facebook_CalmController extends Mage_Core_Controller_Front_Action
+{
+    public function signupAction(){
+    $this->loadLayout();
+    $this->renderLayout();
+    }
+}
