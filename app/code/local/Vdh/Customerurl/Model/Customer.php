2ed88dc11ae4ea02a8bc5aeec4d0f755d300c762
Arush Sehgal <arush@ilovebrands.net>
2011-12-08 06:08:11 +0000

    test

--- app/code/local/Vdh/Customerurl/Model/Customer.php
+++ app/code/local/Vdh/Customerurl/Model/Customer.php
@@ -1,36 +1,41 @@
 <?php
 class Vdh_Customerurl_Model_Customer {
 
-	public function addRewrites($customer) {
+	 public function addRewrites($customer) {
 
-		$helper = Mage::helper('customerurl');
-		
-		$templates = $helper->getMappings();
-		foreach ($templates as $template) {
-			$url = str_replace(
-				array('{customer_id}', '{customer_url}'), 
-				array($customer->getId(), $customer->getCustomerurl()), 
-				$template['url']
-			);
-			$redirect = str_replace(
-				array('{customer_id}', '{customer_url}'), 
-				array($customer->getId(), $customer->getCustomerurl()), 
-				$template['redirect']
-			);
-			$currentUrl = Mage::getModel('core/url_rewrite')->getCollection()
-			->addFilter('target_path', $redirect)
-			->addFilter('id_path', $redirect . '_' . $customer->getId())
-			->getFirstItem();
-			
-			$currentUrl->setStoreId(Mage_Core_Model_App::ADMIN_STORE_ID);
-			$currentUrl->setIdPath($redirect . '_' . $customer->getId());
-			$currentUrl->setRequestPath($url);						
-			$currentUrl->setTargetPath($redirect);			
-			$currentUrl->setIsSystem(false);						
-			$currentUrl->save();
-		}
-		return true;
-	}
+       $helper = Mage::helper('customerurl');
+       
+       $templates = $helper->getMappings();
+       foreach ($templates as $template) {
+           $url = str_replace(
+               array('{customer_id}', '{customer_url}'), 
+               array($customer->getId(), $customer->getCustomerurl()), 
+               $template['url']
+           );
+           $redirect = str_replace(
+               array('{customer_id}', '{customer_url}'), 
+               array($customer->getId(), $customer->getCustomerurl()), 
+               $template['redirect']
+           );
+/*
+           $currentUrl = Mage::getModel('core/url_rewrite')->getCollection()
+           ->addFilter('target_path', $redirect)
+           ->addFilter('id_path', $redirect . '_' . $customer->getId())
+           ->getFirstItem();
+*/
+           $sql = "DELETE FROM ".Mage::getSingleton('core/resource')->getTableName('core_url_rewrite')." WHERE id_path = '".$redirect . '_' . $customer->getId()."' AND target_path = '".$redirect."'";
+           $conn = Mage::getSingleton("core/resource")->getConnection("core_write");
+           $conn->query($sql);
+           $currentUrl = Mage::getModel('core/url_rewrite');
+           $currentUrl->setStoreId(Mage_Core_Model_App::ADMIN_STORE_ID);
+           $currentUrl->setIdPath($redirect . '_' . $customer->getId());
+           $currentUrl->setRequestPath($url);                        
+           $currentUrl->setTargetPath($redirect);            
+           $currentUrl->setIsSystem(false);                        
+           $currentUrl->save();
+       }
+       return true;
+   }
 	
 	public function createUnique($customer, $default = 'username') {
 		if (!$customer->getCustomerurl()) {
