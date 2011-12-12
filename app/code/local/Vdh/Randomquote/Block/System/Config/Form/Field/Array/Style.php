e7ae9692f796cc41fab149a488d45085cbe256b4
Arush Sehgal <arush@ilovebrands.net>
2011-12-12 19:56:50 +0000

    some facebook landing changes;

--- app/code/local/Vdh/Randomquote/Block/System/Config/Form/Field/Array/Style.php
+++ app/code/local/Vdh/Randomquote/Block/System/Config/Form/Field/Array/Style.php
@@ -2,7 +2,7 @@
 
 class Vdh_Randomquote_Block_System_Config_Form_Field_Array_Style extends Mage_Adminhtml_Block_System_Config_Form_Field_Array_Abstract {
     public function __construct() {
-        $this->addColumn('text', 
+        $this->addColumn('text',
 			array(
                 'label'         => Mage::helper('core')->__('Style').' <span class="required">*</span>',
                 'style'         => 'width:350px',
