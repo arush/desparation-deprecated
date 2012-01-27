eeef7b0b46f4447ba686c325c30faddb8a404443
Arush Sehgal <arush@ilovebrands.net>
2012-01-27 18:07:21 +0000

    Revert "added test module with a controller"
    This reverts commit 5a90d6e7fefa673aaca0a5712ba44cdba7e82da7.

--- app/code/local/Arush/Test/controllers/ControllerController.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class Arush_Keep_CalmController extends Mage_Core_Controller_Front_Action
-{
-    public function signmeupAction(){
-    $this->loadLayout();
-    $this->renderLayout();
-    }
-}
