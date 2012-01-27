ba749b6984cea3a44522ee8a0b13797edf4dc6e9
Arush Sehgal <arush@ilovebrands.net>
2012-01-27 18:06:53 +0000

    Revert "added controller to test module"
    This reverts commit f39161d297394e5c7ec95f5f0854be5244e6f459.

--- app/code/local/Arush/Test/controllers/ControllerController.php
+++ app/code/local/Arush/Test/controllers/ControllerController.php
@@ -1,9 +1,9 @@
 <?php
 
-class Arush_Test_ControllerController extends Mage_Core_Controller_Front_Action
+class Arush_Keep_CalmController extends Mage_Core_Controller_Front_Action
 {
-    public function magemonkeyAction(){
-    	Mage::getModel('monkey/cron')->processExportJobs();
-    	echo 'done';
+    public function signmeupAction(){
+    $this->loadLayout();
+    $this->renderLayout();
     }
 }
