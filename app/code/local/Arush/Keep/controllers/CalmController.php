3e49ed46fa460b62a214931131d1cc45f369b6a7
Arush Sehgal <arush@ilovebrands.net>
2012-02-01 15:13:09 +0000

    Revert "removed test module and added action to keep/calm"
    This reverts commit cf53adf6d3a51cac71a7cbc7a3078c7761d1a425.

--- app/code/local/Arush/Keep/controllers/CalmController.php
+++ app/code/local/Arush/Keep/controllers/CalmController.php
@@ -6,9 +6,4 @@ class Arush_Keep_CalmController extends Mage_Core_Controller_Front_Action
     $this->loadLayout();
     $this->renderLayout();
     }
-    
-    public function magemonkeyAction(){
-    	Mage::getModel('monkey/cron')->processExportJobs()
-    
-    }
 }
