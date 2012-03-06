c5421691b3dbd3f159e52993fc22d5b7da3cd3df
Arush Sehgal <arush@ilovebrands.net>
2012-03-06 17:05:28 +0000

    new recurly.js

--- skin/frontend/base/default/js/brainwashed/recurly.js
+++ skin/frontend/base/default/js/brainwashed/recurly.js
@@ -1,11 +1,10 @@
 jQuery.noConflict();
 
 document.observe('dom:loaded', function(){
-
-	$('checkoutSteps').select('.button').each(function(obj){
-		obj.observe('click', function(e){
-			loadRecurly();		
-		})
+	Ajax.Responders.register({
+		onComplete: function(request) {
+			if (request.url.indexOf('checkout/onepage/progress') >= 0) { loadRecurly(); }
+		}
 	});
 
 	$('co-payment-form').on('change', 'input[type="radio"][payment\[method\]]', function(e){
