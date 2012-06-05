9fa3771dc0613eae8a395915d6995d29e33fd7bf
Arush Sehgal <arush@getbrandid.com>
2012-06-05 19:32:57 +0100

    some UX improvements on plan page for choosing upgrades, and made auto select addons work in recurly checkout

--- skin/frontend/default/brandid/js/arush/plan-upgrades.js
+++ skin/frontend/default/brandid/js/arush/plan-upgrades.js
@@ -4,6 +4,23 @@
 * Date: 03 June 2012
 */
 
+// function flushCookies() {
+// 	$j.cookie("upgrade_tees", null);
+// 	$j.cookie("upgrade_shirts", null);
+// }
+
+function addToPrice(a,item) {
+	var currentPrice = parseInt($j('#final-price').text());
+	$j('#final-price').text(currentPrice+a);
+	$j('#'+item+'-text').fadeIn();
+}
+
+function subtractFromPrice(a,item) {
+	var currentPrice = parseInt($j('#final-price').text());
+	$j('#final-price').text(currentPrice-a);
+	$j('#'+item+'-text').fadeOut();
+}
+
 function configurePlan(el) {
 
 	// id is needed to find the unselected item
@@ -11,31 +28,53 @@ function configurePlan(el) {
 	var str = $j(el).parent().parent().attr('class');
 	var selection = el.options[el.selectedIndex];
 
-	if(str.indexOf("tshirt-bg")) {
+	if(str.indexOf("tshirt-bg upgrade") >= 0) {
 		var itemToUpgrade = "upgrade_tees";
 	}
-	else if(str.indexOf("shirt-bg")) {
+	else if(str.indexOf("shirt-bg upgrade") >= 0) {
 		var itemToUpgrade = "upgrade_shirts";
 	}
-	
-	var amountToAdd = parseInt($j(selection).attr('price'));
+	else {
+		opConfig.reloadPrice();
+		return;
+	}
+	//alert("item to upgrade: " + itemToUpgrade + " str: "+str);
+
+	amountToAdd = parseInt($j(selection).attr('price'));
+	// store the index of the selected option
+	var valueToStore = parseInt(
+			$j("#"+dropdownId+" option").index($j("#"+dropdownId+" option:selected"))
+		);
+
 	// if + exists, set the cookie and change the price, else delete it and deduct the price
-	if(amountToAdd > 0) {
-		$j.cookie(itemToUpgrade, amountToAdd);
-		
-		// get amount to add
-		var currentPrice = parseInt($j('#final-price').text());
-		$j('#final-price').text(currentPrice+amountToAdd);
-	} else { // must subtract price when selecting the cheaper option, but first double check cookie exists
+	if(valueToStore < 1) { // "Please select" is selected, so delete cookies
 		if($j.cookie(itemToUpgrade)) {
 			var amountToSubtract = $j.cookie(itemToUpgrade);
-			var currentPrice = parseInt($j('#final-price').text());
-			$j('#final-price').text(currentPrice-amountToSubtract);
-			$j.cookie(itemToUpgrade,0);
+			subtractFromPrice(amountToSubtract,itemToUpgrade);
+
+			$j.cookie(itemToUpgrade,null,{ path: '/'});
+			$j.cookie(itemToUpgrade+"_val",null,{ path: '/'});
+		}
+		return;
+	}
+	else if(amountToAdd > 0) {
+		$j.cookie(itemToUpgrade, amountToAdd,{ path: '/' , expires: 7});
+		$j.cookie(itemToUpgrade+"_val", valueToStore,{ path: '/' , expires: 7});
+		// recalculate price
+		addToPrice(amountToAdd,itemToUpgrade);
+		
+	} else if(amountToAdd === 0) { // must subtract price when selecting the cheaper option, but first double check cookie exists
+		if($j.cookie(itemToUpgrade)) {
+			
+			var amountToSubtract = $j.cookie(itemToUpgrade);
+			subtractFromPrice(amountToSubtract,itemToUpgrade);
+			
+			$j.cookie(itemToUpgrade,0,{ path: '/' , expires: 7});
+			$j.cookie(itemToUpgrade+"_val", valueToStore,{ path: '/' , expires: 7});
 		}
 	}
 
 	// had to remove this from inline js in core hack, replaced with configurePlan, so have to call it now
-	opConfig.reloadPrice();
-
+	
 }
+
