eaccb88f8247e45162f65daa6d67d30e15612e66
Arush Sehgal <arush@getbrandid.com>
2012-06-05 01:40:56 +0100

    added style selector and necessary cookie dropping for plan page

--- skin/frontend/default/brandid/js/arush/plan-upgrades.js
+++ skin/frontend/default/brandid/js/arush/plan-upgrades.js
@@ -1,25 +1,41 @@
+/*
+* Author: Arush Sehgal
+* Purpose: when you select an option for a plan, it stores your choice in a cookie, then when you check out, it adds the correct add-ons in recurly
+* Date: 03 June 2012
+*/
 
 function configurePlan(el) {
 
-	var selection = el.options[el.selectedIndex].value;
+	// id is needed to find the unselected item
+	var dropdownId = $j(el).attr('id');
+	var str = $j(el).parent().parent().attr('class');
+	var selection = el.options[el.selectedIndex];
 
-	switch (selection) {
-		
-		//when user upgrades, set the cookie to true
-		case "Polo":
-		  $j.cookie('upgrade_tees', true);
-		  break;
-		case "Saturday Night":
-		  $j.cookie('upgrade_shirts', true);
-		  break;
-
-		//when user downgrades, delete the cookie
-		case "crew-vneck":
-		  $j.cookie('upgrade_tees', null);
-		  break;
-		case "Monday Morning":
-		  $j.cookie('upgrade_shirts', null);
-		  break;
+	if(str.indexOf("tshirt-bg")) {
+		var itemToUpgrade = "upgrade_tees";
 	}
+	else if(str.indexOf("shirt-bg")) {
+		var itemToUpgrade = "upgrade_shirts";
+	}
+	
+	var amountToAdd = parseInt($j(selection).attr('price'));
+	// if + exists, set the cookie and change the price, else delete it and deduct the price
+	if(amountToAdd > 0) {
+		$j.cookie(itemToUpgrade, amountToAdd);
+		
+		// get amount to add
+		var currentPrice = parseInt($j('#final-price').text());
+		$j('#final-price').text(currentPrice+amountToAdd);
+	} else { // must subtract price when selecting the cheaper option, but first double check cookie exists
+		if($j.cookie(itemToUpgrade)) {
+			var amountToSubtract = $j.cookie(itemToUpgrade);
+			var currentPrice = parseInt($j('#final-price').text());
+			$j('#final-price').text(currentPrice-amountToSubtract);
+			$j.cookie(itemToUpgrade,0);
+		}
+	}
+
+	// had to remove this from inline js in core hack, replaced with configurePlan, so have to call it now
+	opConfig.reloadPrice();
 
 }
