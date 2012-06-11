10dd1b93521d2c5e66d992baa187794876546107
Arush Sehgal <arush@getbrandid.com>
2012-06-11 01:28:59 +0100

    skeleton template and responsive who page created

--- app/code/local/Arush/Keep/controllers/CalmController.php
+++ app/code/local/Arush/Keep/controllers/CalmController.php
@@ -16,4 +16,9 @@ class Arush_Keep_CalmController extends Mage_Core_Controller_Front_Action
     $this->loadLayout();
     $this->renderLayout();
     }
+
+    public function weareprosAction(){
+    $this->loadLayout();
+    $this->renderLayout();
+    }
 }
