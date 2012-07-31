8747836f3b22e53b2e4a5e24e8eab58c318a40b9
Arush Sehgal <arush@ilovebrands.net>
2012-07-31 18:31:49 +0100

    fixed recurly incorrectly emptying the cart

--- app/code/local/Brainwashed/Recurly/Model/Product/Observer.php
+++ app/code/local/Brainwashed/Recurly/Model/Product/Observer.php
@@ -2,32 +2,32 @@
 
 class Brainwashed_Recurly_Model_Product_Observer {
 	
-	private function _clearCart($sku) {
-		$quote = Mage::getSingleton('checkout/session')->getQuote();
-        foreach ($quote->getAllItems() as $item) {
-        	if ($item->getSku() == $sku) { continue; }
-            Mage::getSingleton('checkout/cart')->removeItem($item->getId());
-        }
-        Mage::getSingleton('checkout/cart')->save();
-	}
-
-	public function redirectToCheckout($observer) {
-
+	public function recurlyRedirectToCheckout($observer) {
+		
 		$product = $observer->getEvent()->getProduct();
-
-    	$planCodes = array();
+		
+		$planCodes = array();
     	$plans = Mage::helper('recurly')->getPlans();
     	foreach($plans as $plan){
     		$planCodes[] = $plan->plan_code;
     	}
-		if (!in_array($product->getSku(), $planCodes)) { return; } // We only empty cart for a Recurly Subscription
+		if (!in_array($product->getSku(), $planCodes)) { return; } // We only redirect cart for a Recurly Subscription
 
-		$this->_clearCart($product->getSku());
-
-	    $response = $observer->getResponse();
+		$response = $observer->getResponse();
 	    $request = $observer->getRequest();
 	    $response->setRedirect(Mage::helper('checkout/url')->getCheckoutUrl());
 	    Mage::getSingleton('checkout/session')->setNoCartRedirect(true);
+
+	}
+
+	public function emptyCart($observer) {
+		
+		$quote = Mage::getSingleton('checkout/session')->getQuote();
+
+        foreach ($quote->getAllItems() as $item) {
+            Mage::getSingleton('checkout/cart')->removeItem($item->getId());
+        }
+        Mage::getSingleton('checkout/cart')->save();
 
 	}
 }
