b36fc93e5cb8f4ebc1f4743cb4c715668aa10311
Arush Sehgal <arush@ilovebrands.net>
2012-08-10 17:25:19 +0100

    testing geckoboard

---
+++ geck/mailchimp.php
@@ -0,0 +1,52 @@
+<?php
+
+require_once('core.php');
+
+
+// first of the month
+// $ts = date("Y-m-d H:i:s", mktime(0, 0, 0, date('m'), 1, date('Y')));
+// $te = date('Y-m-d H:i:s', mktime(date('H'), date('i'), date('s'), date('m'), date('d'), date('Y')));
+
+// $ys = date("Y-m-d H:i:s", mktime(0, 0, 0, date('m')-1, 1, date('Y')));
+// $ye = $ts;
+
+// compare with this time a month ago
+$ts = date('Y-m-d H:i:s', strtotime('-1 month'));
+$te = date('Y-m-d H:i:s', mktime(date('H'), date('i'), date('s'), date('m'), date('d'), date('Y')));
+
+$ys = date('Y-m-d H:i:s', strtotime('-1 month',strtotime('-1 month')));
+$ye = date('Y-m-d H:i:s', mktime(date('H'), date('i'), date('s'), date('m')-1, date('d'), date('Y')));
+
+
+// if (isset($_POST) && isset($_SERVER['PHP_AUTH_USER'])) {
+
+
+// 	/* Check API key */
+//     if ('XXXX' == $_SERVER['PHP_AUTH_USER']) {
+		$mailchimpData = getMaleSegment($ts,'check');
+		
+
+		// $ordersRefunded = array("value"=>$countAtClosed, "text"=>"Orders Refunded");
+		// $ordersProcessing = array("value"=>$countAtProcessing, "text"=>"Orders Processing");
+		// $ordersComplete = array("value"=>$countAtComplete, "text"=>"Orders Invoiced");
+
+		// $response = array("item"=>array($ordersRefunded,$ordersProcessing,$ordersComplete));
+
+
+		$json = json_encode($mailchimpData);
+
+		echo $json;        
+    
+//     } else {
+//         Header("HTTP/1.1 403 Access denied");
+//         $data = array('error' => 'Nice try, asshole.');
+//         echo $data;
+//     }
+
+// } else {
+// 	Header("HTTP/1.1 404 Page not found");
+// }
+
+
+
+?>
