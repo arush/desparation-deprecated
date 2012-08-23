55be42a29df940680d80373ff922d52fdd40084b
ILOVEBRANDSOUTLET WEBSITE <liveserver@ilovebrands.net>
2012-08-23 13:43:12 +0000

    Revert "added IP address"
    This reverts commit 3fd4947f13807a7e486764269f848e5ebfc0697d.

--- join-the-family.php
+++ join-the-family.php
@@ -2,35 +2,34 @@
 
 if (isset($_POST)) {
 	echo 'we\'re taking a break from hiring at the minute, come back later';
-	$ip = $_SERVER['REMOTE_ADDR'];
+	
+	// 	// get post parameters
+	// 	$from = $_POST['email_ad'];
+	// 	$twitter = $_POST['twitter_handle'];
+	// 	$github = $_POST['github_username'];
+	// 	$message = 'Hi I\'d like to build an empire with you. Github: ' . $github . ' Twitter: ' . $twitter;
 
-		// get post parameters
-		$from = $_POST['email_ad'];
-		$twitter = $_POST['twitter_handle'];
-		$github = $_POST['github_username'];
-		$message = 'My IP is: ' . $ip . 'Github: ' . $github . ' Twitter: ' . $twitter;
+	// /* Check API key */
+ //    // if ($apiKey == $_SERVER['PHP_AUTH_USER']) {
 
-	/* Check API key */
-    // if ($apiKey == $_SERVER['PHP_AUTH_USER']) {
-
-		$to = "founders@getbrandid.com";
-		$subject = "Job Application via M.A.L.E.";
-		// $from = "someonelse@example.com";
-		$headers = "From:" . $from . "\r\n" .
-					"Cc:" . $from;
+	// 	$to = "founders@getbrandid.com";
+	// 	$subject = "Job Application via M.A.L.E.";
+	// 	// $from = "someonelse@example.com";
+	// 	$headers = "From:" . $from . "\r\n" .
+	// 				"Cc:" . $from;
 		
-		mail($to,$subject,$message,$headers);
+	// 	mail($to,$subject,$message,$headers);
 
-		$email = array();
-		$email['to'] = $to;
-		$email['subject'] = $subject;
-		$email['message'] = $message;
-		$email['from'] = $from;
+	// 	$email = array();
+	// 	$email['to'] = $to;
+	// 	$email['subject'] = $subject;
+	// 	$email['message'] = $message;
+	// 	$email['from'] = $from;
 
-        $retval = array('status' => 'success', 'response' => 'M.A.L.E. here, I\'ve sent your application through, thanks. Remember to tweet @getbrandid to follow up.', 'payload' => $email);
-		$json = json_encode($retval);
+ //        $retval = array('status' => 'success', 'response' => 'M.A.L.E. here, I\'ve sent your application through, thanks. Remember to tweet @getbrandid to follow up.', 'payload' => $email);
+	// 	$json = json_encode($retval);
 
-		// echo $json;  
+	// 	echo $json;  
 
 
  //    // } else {
