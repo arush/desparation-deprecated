d2b8db68d07d53542f3580a842c6bed39c8b5245
Arush Sehgal <arushsehgal@Arushs-MacBook-Air.local>
2012-08-23 13:18:16 +0100

    updated job application with post variables

--- join-the-family.php
+++ join-the-family.php
@@ -1,20 +1,32 @@
 <?php
 
-// if (isset($_POST) && isset($_SERVER['PHP_AUTH_USER'])) {
+if (isset($_POST) && isset($_SERVER['PHP_AUTH_USER'])) {
 
+
+		// get post parameters
+		$from = $_POST['email_ad'];
+		$message = $_POST['your_message'];
+		$twitter = $_POST['twitter_handle'];
+		$message += '\n\n' + 'Twitter: ' + $twitter;
 
 	/* Check API key */
     // if ($apiKey == $_SERVER['PHP_AUTH_USER']) {
 
-		$to = "jobs@getbrandid.com";
-		$subject = "Test mail";
-		$message = "Hello! This is a simple email message.";
-		$from = "someonelse@example.com";
+		$to = "founders@getbrandid.com";
+		$subject = "Job Application via M.A.L.E.";
+		// $from = "someonelse@example.com";
 		$headers = "From:" . $from;
+		
 		mail($to,$subject,$message,$headers);
 
+		$email = array();
+		$email['to'] = $to;
+		$email['subject'] = $subject;
+		$email['message'] = $message;
+		$email['from'] = $from;
+		$email['headers'] = $headers;
 
-        $retval = array('status' => 'success', 'message' => 'M.A.L.E. here, I\'ve sent your application through to Arush, thanks for applying.');
+        $retval = array('status' => 'success', 'response' => 'M.A.L.E. here, I\'ve sent your application through to Arush, thanks for applying. Remember to tweet @getbrandid to follow up.', 'payload' => $email);
 		$json = json_encode($retval);
 
 		echo $json;  
@@ -26,11 +38,12 @@
     //     echo $data;
     // }
 
-// } else {
-// 	$sorry = array();
-// 	$sorry['computer says'] = 'no';
-// 	echo json_encode($sorry);
-// }
+} else {
+	$sorry = array();
+	$sorry['status'] = 'sorry';
+	$sorry['computer says'] = 'no';
+	echo json_encode($sorry);
+}
 
 
 ?>
