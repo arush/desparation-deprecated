9b01f96c58c27be2f42ae24c0c51d98dec7e9fbc
Arush Sehgal <arush@ilovebrands.net>
2012-11-06 18:28:27 +0000

    completed version 1 of app page

--- twilio/download-app.php
+++ twilio/download-app.php
@@ -14,6 +14,7 @@
 	// Step 1: Download the Twilio-PHP library from twilio.com/docs/libraries, 
 	// and move it into the folder containing this file.
 	require "../lib/Twilio/Services/Twilio.php";
+	require_once('magento-twilio.php');
 
 	// Step 2: set our AccountSid and AuthToken from www.twilio.com/user/account
 	$AccountSid = "ACe27c18b2600c4d14980fc5549e4c9cdb";
@@ -34,21 +35,75 @@
 	// Step 5: Loop over all our friends. $number is a phone number above, and 
 	// $name is the name next to it
 	foreach ($people as $number => $name) {
+		if($email = isPayingCustomer($number)) {
 
-		$sms = $client->account->sms_messages->create(
+			$message1 = "(1/3) M.A.L.E. here. I have your number linked to: " . $email . ", and looks like you are ready to test our order-by-mobile system.";
 
-		// Step 6: Change the 'From' number below to be a valid Twilio number 
-		// that you've purchased, or the (deprecated) Sandbox number
-			"442033222520", 
+			$sms1 = $client->account->sms_messages->create(
 
-			// the number we are sending to - Any phone number
-			$number,
+			// Step 6: Change the 'From' number below to be a valid Twilio number 
+			// that you've purchased, or the (deprecated) Sandbox number
+				"442033222520", 
 
-			// the sms body
-			"Hey $name, Monkey Party at 6PM. Bring Bananas!"
-		);
+				// the number we are sending to - Any phone number
+				$number,
 
-		// Display a confirmation message on the screen
-		$done = array("status" => "complete");
-		echo json_encode($done);
+				// the sms body
+				$message1
+			);
+
+			$message2 = "(2/3) but before we release the app, I'll just send txts instead of push notifications, so we can get your feedback on what to build in the app.";
+
+			$sms2 = $client->account->sms_messages->create(
+
+			// Step 6: Change the 'From' number below to be a valid Twilio number 
+			// that you've purchased, or the (deprecated) Sandbox number
+				"442033222520", 
+
+				// the number we are sending to - Any phone number
+				$number,
+
+				// the sms body
+				$message2
+			);
+
+			$message3 = "(3/3) Your first reminder will arrive by txt soon. All will be explained. As always call BRANDiD support on 0203 322 2882 with your feedback. Over & Out.";
+
+			$sms3 = $client->account->sms_messages->create(
+
+			// Step 6: Change the 'From' number below to be a valid Twilio number 
+			// that you've purchased, or the (deprecated) Sandbox number
+				"442033222520", 
+
+				// the number we are sending to - Any phone number
+				$number,
+
+				// the sms body
+				$message3
+			);
+			$done = array("status" => "1", "message" => "Success! Check your txt messages for further instructions.");
+			echo json_encode($done);
+
+		} else {
+			$sms = $client->account->sms_messages->create(
+
+			// Step 6: Change the 'From' number below to be a valid Twilio number 
+			// that you've purchased, or the (deprecated) Sandbox number
+				"442033222520", 
+
+				// the number we are sending to - Any phone number
+				$number,
+
+				// the sms body
+				"M.A.L.E. here. Looks like you don't have a plan yet. You'll need to set one up at getbrandid.com first, then try and download the app again."
+			);
+
+			// Display a confirmation message on the screen
+			$done = array("status" => "2", "message" => "Looks like you haven't purchased from BRANDiD before. You'll need to do that first, then come back and get the app.");
+			echo json_encode($done);
+		}
+
+		
+
+		
 	}
